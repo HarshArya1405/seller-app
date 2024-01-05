44a95a1b7ec568f9d4947cc16d9f8df2394283d2
Tirthankar007 <roytirthankar2@gmail.com>
2024-01-05 15:59:44 +0530

    add, added the customization controller

---
+++ seller/app/modules/customization/controllers/customizationController.js
@@ -0,0 +1,48 @@
+import CustomizationService from '../v1/services/customizationService';
+
+const customizationService = new CustomizationService();
+
+class CustomizationController {
+    async storeCustomizationsGroup(req, res, next) {
+        try {
+            const data = req.body;
+            const categoryVariant = await customizationService.createCustomizationGroups(data.customizationDetails, req.user);
+            return res.send(categoryVariant);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async getCustomizationsGroup(req, res, next) {
+        try {
+            const currentUser = req.user;
+            const customizationGroups = await customizationService.getCustomizationGroups(currentUser);
+            return res.json({ customizationGroups });
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async updateCustomizationsGroup(req, res, next) {
+        try {
+            const currentUser = req.user;
+            const { customizationDetails } = req.body;
+            const updateResult = await customizationService.updateCustomizationGroups(customizationDetails, currentUser);
+            return res.send(updateResult);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async deleteCustomizationGroup(req, res, next) {
+        try {
+            const currentUser = req.user;
+            const deleteResult = await customizationService.deleteCustomizationGroups(currentUser);
+            return res.send(deleteResult);
+        } catch(error) {
+            next(error);
+        }
+    }
+}
+
+export default CustomizationController;
